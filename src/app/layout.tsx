545bfd81a505fec0ff239ca98c1e8cdb37063610
ashish <ashish7232sing@gmail.com>
2025-07-21 10:47:04 +0530

    as

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import './Style.css'
 import Header from "./components/Navigation/Header";
 import HamburgerMenu from "./components/Navigation/compleateHeader";
 import Footer from "./components/Footer";
